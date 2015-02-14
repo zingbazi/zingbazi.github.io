28a0b2a0fe79c0fffa62dff9c4abc52f12f33db9
Ubuntu <ubuntu@ip-10-191-46-149.ec2.internal>
2015-02-14 15:24:58 +0000

    first commit

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+	$('#top-slider ul a').click(function () {
+	
+		//reset all the items
+		$('.customBlock ul a').removeClass('active');
+		
+		//set current item as active
+		$(this).addClass('active');	
+		
+		//scroll it to the right position
+		$('.mask').scrollTo($(this).attr('rel'), 300);
+		
+		//disable click event
+	    return false;		
+		
+	});
+	
+});
